0e2c380d3ad6236bf07d4c0c4f07c315ec313b03
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-02-14 17:46:06 +0100

    Update PersonController.java

--- src/main/java/com/example/betriebslandkarte/Person/PersonController.java
+++ src/main/java/com/example/betriebslandkarte/Person/PersonController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:3000") // Specify the allowed origin
+@CrossOrigin// Specify the allowed origin: allow all origins
 @RestController
 @RequestMapping(path = "api/v1/map")
 public class PersonController {
